46dce1bb09bbd3e48aea0d6876669064d5f227bf
Sahoo108 <sahoo.pradyumna26@gmail.com>
2024-08-20 20:51:24 +0530

    first-commit

---
+++ playground/src/main/java/com/sahoo/playground/Logger.kt
@@ -0,0 +1,32 @@
+package com.sahoo.playground
+
+import android.util.Log
+
+
+const val isDebugEnabled = true
+
+
+fun <T> logDebug(tag:String, value:T){
+    if(!isDebugEnabled) return
+    Log.e("DEBUG=> $tag", value.toString())
+}
+
+fun <T> logInfo(tag:String, value:T){
+    if(!isDebugEnabled) return
+    Log.e("INFO=> $tag", value.toString())
+}
+
+fun <T> logError(tag:String, value:T){
+    if(!isDebugEnabled) return
+    Log.e("ERROR=> $tag", value.toString())
+}
+
+fun <T> logView(tag:String, value:T){
+    if(!isDebugEnabled) return
+    Log.v("VIEW=> $tag", value.toString())
+}
+
+fun <T> logWarn(tag:String, value:T){
+    if(!isDebugEnabled) return
+    Log.v("WARN=> $tag", value.toString())
+}
